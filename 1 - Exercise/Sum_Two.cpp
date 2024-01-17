9f8c816c14d8deb8617b9349f9ff6ad25f3b8653
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2024-01-17 20:35:49 +0100

    Corredted Sum_two in C++

--- 1 - Exercise/Sum_Two.cpp
+++ 1 - Exercise/Sum_Two.cpp
@@ -7,7 +7,7 @@ public:
 
         for (int i = 0;i < nums.size();i++){
 
-            if(mp.find(target - nums[i] != mp.end()){
+            if(mp.find(target - nums[i]) != mp.end()){
                 ans.push_back(i);
                 ans.push_back( mp[target - nums[i]] );
                 break;
